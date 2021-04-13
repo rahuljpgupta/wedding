dfd44e19419e3084bb4d388450a58de69a44ef72
Rahul Gupta <rahuljpgupta@gmail.com>
2021-04-13 10:46:16 +0530

    enable audio

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@
       $('.sakura-falling').sakura();
 })(jQuery);
 
-// $(document).on('click', function(){
-//     document.getElementById("my_audio").play();
-//     console.log('Shaadi me zaroor aana');
-// });
+$(document).on('click', function(){
+    document.getElementById("my_audio").play();
+    console.log('Shaadi me zaroor aana');
+});
 
 // Set the date we're counting down to
 var countDownDate = new Date("Feb 16, 2021 20:00:00").getTime();
